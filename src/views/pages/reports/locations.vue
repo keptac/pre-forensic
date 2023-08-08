7df120a22f179db7a4a17810db5b7b9561312888
Kelvin Chelenje <keptac.dev@gmail.com>
2023-08-08 13:19:56 +0200

    world globe heat map

--- src/views/pages/reports/locations.vue
+++ src/views/pages/reports/locations.vue
@@ -2,6 +2,7 @@
 import Layout from "../../layouts/main";
 import PageHeader from "@/components/page-header";
 import LocationsTable from './locationsTable';
+import MapChart from 'vue-map-chart'
 
 
 export default {
@@ -9,13 +10,18 @@ export default {
     title: "Locations",
     meta: [{ name: "description" }]
   },
-  components: { Layout, PageHeader, LocationsTable },
+  components: { Layout, PageHeader, LocationsTable, MapChart },
   data() {
     return {
       title: "Locations",
       items: [
         ""
-      ]
+      ],
+      countryData: {
+        "ZA": 150,
+        "ZW": 2,
+        "ZM": 0,
+      }
     };
   }
 };
@@ -24,18 +30,25 @@ export default {
 <template>
   <Layout>
     <PageHeader class="mt-5 ml-2" :title="title" :items="items" />
-    <div class="row">
-        <div class="col-lg-12 ">
+
+      <div class="row">
+        <div class="col-lg-5">
+            <LocationsTable/>
+        </div>
+
+        <div class="col-lg-7 ">
           <div class="card">
             <div class="card-body">
-              <p class="text-center">Locations Visual Map Coming soon</p>
+              <p class="text-center">Locations Visual Map</p>
+              <MapChart
+                :countryData="countryData"
+                highColor="#940101"
+                lowColor="#fccfcf"
+                countryStrokeColor="#909090"
+                defaultCountryFillColor="#dadada"
+                />
             </div>
           </div>
-        </div>
-    </div>
-      <div class="row">
-        <div class="col-lg-12 ">
-            <LocationsTable/>
         </div>
     </div>
   </Layout>
--- src/views/pages/reports/locationsTable.vue
+++ src/views/pages/reports/locationsTable.vue
@@ -12,29 +12,9 @@ import {
 export default {
   data() {
     return {
-      locationsData: [
-        { "id":"1",
-          "country":"South Africa", 
-          "city":"Johannesburg ", 
-          "count": 150
-        },
-      ],
-      expandedLocationsData: [],
+      locationsData: [],
       variableObject:{},
       transactionsObject:[],
-
-      mtotalRows: 1,
-      showExpanded: false,
-      mcurrentPage: 1,
-      mperPage: 50,
-      mpageOptions: [5, 10, 25],
-      mfilter: null,
-      mfilterOn: [],
-      msortBy: "id",
-      msortDesc: false,
-
-
-      expandedLocationTitle:"",
       totalRows: 1,
       currentPage: 1,
       perPage: 50,
@@ -56,21 +36,6 @@ export default {
         { field: "regcityistrationNumber", label:  "City" },
         { field: "count", label: "Count" },
       ],
-
-
-      expandedFields: [
-      { key: "id", sortable: true, label: "id" },
-        { key: "country", sortable: true, label: "Country" },
-        { key: "city",sortable: true, label: "City" },
-        { key: "count",sortable: true, label: "Count" },
-    ],
-
-      mcolumns: [
-      { field: "id", label: "id" },
-        { field: "country", label: "Country" },
-        { field: "regcityistrationNumber", label:  "City" },
-        { field: "count", label: "Count" },
-      ],
       
     };
   },
@@ -86,11 +51,6 @@ export default {
     rows() {
       return this.locationsData.length;
     },
-
-    mrows() {
-      return this.transactionsObject.length;
-    },
-
     notification() {
         return this.$store ? this.$store.state.notification : null;
       }
@@ -112,11 +72,6 @@ export default {
       this.currentPage = 1;
     },
 
-    monFiltered(filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.mtotalRows = filteredItems.length;
-      this.mcurrentPage = 1;
-    },
 
     async loadAllLocations() {
         try {
@@ -128,37 +83,16 @@ export default {
         } catch (error) {
           console.log(error);
         }
+
+        this.locationsData =[
+        { "id":"1",
+          "country":"South Africa", 
+          "city":"Johannesburg ", 
+          "count": 150
+        },
+      ];
     },
 
-    closeLocation(){
-      this.expandedLocationsData = [];
-      this.showExpanded = false;
-
-    },
-
-    async expandKeyLocation(title, id){
-      this.showExpanded = true;
-      this.expandedLocationTitle = title;
-  
-        try {
-          await accountsService.getLocationsByIdentifier(id).then(response=>{
-            if(response.responseBody.length>0){
-                this.expandedLocationsData = response.responseBody;
-              }
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        this.expandedLocationsData =[
-          { "id":"1",
-            "country":"South Africa", 
-            "city":"Johannesburg ", 
-            "count": 150
-          },
-        ]
-
-    },
   }
 };
 </script>
@@ -166,7 +100,7 @@ export default {
 <template>
 
   <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-12">
       <div class="card">
         <div class="card-body">
           <div class="row mt-4"> 
@@ -207,18 +141,6 @@ export default {
               :filter-included-fields="filterOn"
               @filtered="onFiltered"
             >
-              <template v-slot:cell(count)="row">
-                  <a
-                    @click="expandKeyLocation(row.item.id, row.item.count)"
-                    href="javascript:void(0);"
-                    class="mr-3 text-primary"
-                    v-b-tooltip.hover
-                    title="Click to view"
-                  >
-                 {{row.item.count}}
-
-                  </a>
-              </template>
             </b-table>
           </div>
           <div class="row">
@@ -235,84 +157,5 @@ export default {
       </div>
     </div>
     
-    <div v-if="showExpanded" class="col-md-6">
-      <div class=" card">
-        <div class="card-body">
-
-          <div class="row">
-            <i class=" ri-close-circle-fill font-size-24 ml-2 mr-2"  @click="closeLocation()"></i>
-            <h4 class="card-title mt-2">{{ expandedLocationTitle }}</h4>
-          </div>
-
-          <div class="row mt-4">
-            <div class="col-sm-12 col-md-4">
-              <div id="tickets-table_length" class="dataTables_length">
-                <label class="d-inline-flex align-items-center">
-                  Show&nbsp;
-                  <b-form-select v-model="mperPage" size="sm" :options="mpageOptions"></b-form-select>&nbsp;entries
-                </label>
-              </div>
-            </div>
-            <!-- Search -->
-            <div class="col-sm-12 col-md-7">
-              <div id="tickets-table_filter" class="dataTables_filter text-md-right">
-                <label class="d-inline-flex align-items-center">
-                  Search:
-                  <b-form-input
-                    v-model="mfilter"
-                    type="search"
-                    class="form-control form-control-sm ml-2"
-                  ></b-form-input>
-                </label>
-              </div>
-            </div>
-            <!-- End search -->
-
-            <b-dropdown right toggle-class="arrow-none card-drop" class="col-md-1 float-right" variant="white">
-              <template v-slot:button-content>
-                <i class="mdi mdi-dots-vertical"></i>
-              </template>
-              <b-dropdown-item>
-                <vue-excel-xlsx
-                    class="btn"
-                    :data="expandedLocationsData"
-                    :columns="mcolumns"
-                    :file-name="'Locations'"
-                    :file-type="'xlsx'"
-                    :sheet-name="'All Locations'"
-                    >
-                    Export
-                </vue-excel-xlsx>
-              </b-dropdown-item>
-              </b-dropdown>
-          </div>
-          <div class="table-responsive">
-            <b-table
-              :items="expandedLocationsData"
-              :fields="expandedFields"
-              responsive="sm"
-              :per-page="mperPage"
-              :current-page="mcurrentPage"
-              :sort-by.sync="msortBy"
-              :sort-desc.sync="msortDesc"
-              :filter="mfilter"
-              :filter-included-fields="mfilterOn"
-              @filtered="monFiltered"
-            >
-            </b-table>
-          </div>
-          <div class="row">
-            <div class="col">
-              <div class="dataTables_paginate paging_simple_numbers float-right">
-                <ul class="pagination pagination-rounded mb-0">
-                  <!-- pagination -->
-                  <b-pagination v-model="mcurrentPage" :total-rows="mrows" :per-page="mperPage"></b-pagination>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
